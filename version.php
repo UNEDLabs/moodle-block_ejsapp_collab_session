34fc4fe45f01ac3fb77e0dd33ee33a957b45396a
Luis de la Torre <ldelatorre@dia.uned.es>
2012-12-21 20:24:22 +0100

    Updating requirements (v. 1.1)

--- version.php
+++ version.php
@@ -32,10 +32,10 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2012121100;
+$plugin->version = 2012122100;
 $plugin->requires = 2010112400;
 $plugin->cron = 0;
 $plugin->component = 'block_ejsapp_collab_session'; // To check on upgrade, that module sits in correct place
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.1 (Build: 2012112800)';
-$plugin->dependencies = array('mod_ejsapp' => 2012092400);
+$plugin->dependencies = array('mod_ejsapp' => 2012121800);
